171f1ef0a7ca4237951868759a0ea1a1abf5babe
moon8585009 <music1000009@gmail.com>
2026-09-07 23:23:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,8 +57,8 @@ class Config:
         self.VIDEO_MAX_HEIGHT: int = self._parse_video_height()
 
         # ArtistBots API @ArtistApibot
-        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://api01.shrutibots.site")
-        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "ShrutiBotsBUvGvLipr11ROSbBMXuG")
+        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://music.artistbots.workers.dev")
+        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "Artistbots")
         self.ENABLE_API: bool = self._str_to_bool(getenv("ENABLE_API", "True"))
         self.ENABLE_COOKIES_FALLBACK: bool = self._str_to_bool(getenv("ENABLE_COOKIES_FALLBACK", "True"))
         self.API_TIMEOUT: int = int(getenv("API_TIMEOUT", "60"))
